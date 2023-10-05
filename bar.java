3bfcbc13d871a2aafe31c62a186ed77b395aad0f
Anushaanubyalla <byallaanu860@gmail.com>
2023-10-05 11:03:10 +0530

    added java and python programms

---
+++ bar.java
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.bar([ 1,3,5,7,9],[5,2,7,8,2],label="datal")
+plt.legend()
+plt.xlabel('xvalues')
+plt.ylabel('height')
+plt.title('verticl barchart')
+plt.show()
